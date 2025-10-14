82049a510fc6d09190a95284a3cadf7fdad6d9ff
Ye Joo Park <subwaymatch@gmail.com>
2025-10-14 10:58:41 -0500

    Set baseUrl, disable legacy-peer-deps with npm

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/cpa-far-book/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -100,7 +100,7 @@ const config: Config = {
           items: [
             {
               label: "Tutorial",
-              to: "/",
+              to: "/docs/intro",
             },
           ],
         },
