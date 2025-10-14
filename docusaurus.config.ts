1cea526dd05a3a128c4683e7c968e85d1f9706e5
Ye Joo Park <subwaymatch@gmail.com>
2025-10-14 11:02:31 -0500

    Fix baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -100,7 +100,7 @@ const config: Config = {
           items: [
             {
               label: "Tutorial",
-              to: "/docs/intro",
+              to: "/",
             },
           ],
         },
